8851717c40df9ea0ba6bf4e1e69f09699947854c
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-17 08:52:13 +0900

    Time: 374 ms (50.38%), Space: 34.9 MB (12.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,18 +4,19 @@ class Solution:
         max = 0
 
         for n in nums:
-            dict[n] = True
-        
+            dict[n] = 0
+
         for n in dict.keys():
             if n - 1 not in dict.keys():
                 count = 0
                 cur_num = n
-
+                
                 while cur_num in dict.keys():
                     count += 1
                     cur_num += 1
-            
+
                 if max < count:
                     max = count
-        
+
+
         return max
