008ddb21c1fcece79c8dece7ba18d7aec4cbdb5c
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-16 16:23:28 +0900

    Time: 55 ms (81.13%), Space: 17.9 MB (10.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        list = sorted(nums)
-        left = 0
-        right = len(list) - 1
-        result = []
+        list = {}
 
-        while left < right:
-            calc = list[left] + list[right]
+        for idx, key in enumerate(nums):
+            req = target - key
 
-            if calc > target:
-                right -= 1
-            
-            elif calc < target:
-                left += 1
-            
-            else:
-                break
+            if req in list.keys():
+                return [list[req], idx]
 
-        for idx, num in enumerate(nums):
-            if num == list[left] or num == list[right]:
-                result.append(idx)
-
-        result.sort()
-        return result
+            list[key] = idx
