8a8b2d4c79f397f2121e7adb6ac268a4fac027e2
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-25 14:22:30 +0900

    Time: 42 ms (44.28%), Space: 17.6 MB (67%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -11,13 +11,13 @@ class Solution:
         q.append((root, 1))
 
         while q:
-            cur_node = q.popleft()
-            depth.append(cur_node[1])
+            cur_node, cur_depth = q.popleft()
+            depth.append(cur_depth)
 
-            if cur_node[0].left:
-                q.append((cur_node[0].left, cur_node[1] + 1))
+            if cur_node.left:
+                q.append((cur_node.left, cur_depth + 1))
 
-            if cur_node[0].right:
-                q.append((cur_node[0].right, cur_node[1] + 1))
+            if cur_node.right:
+                q.append((cur_node.right, cur_depth + 1))
 
         return max(depth)
