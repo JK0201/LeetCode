cabb3854113737abb2344d9a95868031aaf1cb3f
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-29 16:38:18 +0900

    Time: 23 ms (98.72%), Space: 16.5 MB (15.95%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,3 +1,27 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace('()', 'o').replace('(al)', 'al')
+        res = ''
+        stack = []
+
+        for c in command:
+            if c == '(':
+                stack.append(')')
+
+            elif c == ')' and stack[-1] == c:
+                stack.pop()
+                stack.append('o')
+                res += 'o'
+
+            elif c == 'a' and stack[-1] == ')':
+                stack.pop()
+                stack.append(c)
+                res += c
+
+            elif c == ')' and stack[-1] == 'l':
+                pass
+
+            else:
+                stack.append(c)
+                res += c
+
+        return res
