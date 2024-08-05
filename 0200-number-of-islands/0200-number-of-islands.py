ea79a7fc4c37944425f2d061baf9cebeeb1ca655
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-05 18:16:26 +0900

    Time: 247 ms (53.98%), Space: 18.9 MB (57.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,33 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         cnt = 0
-        row = len(grid)
-        col = len(grid[0])
+        row, col = len(grid), len(grid[0])
         visited = [[False] * col for _ in range(row)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        def dfs(cur_r, cur_c):
-            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-            visited[cur_r][cur_c] = True
+        def bfs(r, c):
+            visited[r][c] = True
+            q = deque()
+            q.append((r, c))
 
-            for dr, dc in directions:
-                next_r = cur_r + dr
-                next_c = cur_c + dc
+            while q:
+                cur_r, cur_c = q.popleft()
+                
+                for dr, dc in directions:
+                    next_r = cur_r + dr
+                    next_c = cur_c + dc
 
-                if 0 <= next_r < row and 0 <= next_c < col:
-                    if grid[next_r][next_c] == '1' and not visited[next_r][next_c]:
-                        dfs(next_r, next_c)
+                    if 0 <= next_r < row and 0 <= next_c < col:
+                        if grid[next_r][next_c] == '1' and not visited[next_r][next_c]:
+                            visited[next_r][next_c] = True
+                            q.append((next_r, next_c))
 
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == '1' and not visited[r][c]:
-                    dfs(r, c)
+                    bfs(r, c)
                     cnt += 1
 
         return cnt
