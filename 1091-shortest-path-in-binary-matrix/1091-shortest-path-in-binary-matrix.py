e917a911790016d416f86ca66b4bbb4750958946
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-05 16:09:24 +0900

    Time: 385 ms (86.71%), Space: 16.9 MB (85.24%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,30 +5,29 @@ class Solution:
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
 
-        row = len(grid)
-        col = len(grid[0])
+        shortest_path = -1
+        row, col = len(grid), len(grid[0])
         visited = [[False] * col for _ in range(row)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
-        def bfs(r, c):
-            directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-            visited[r][c] = True
-            q = deque()
-            q.append((r, c, 1))
+        q = deque()
+        q.append((0, 0, 1))
+        visited[0][0] = True
 
-            while q:
-                cur_r, cur_c, dist = q.popleft()
-                
-                if cur_r == row -1 and cur_c == col -1:
-                    return dist
-                
-                for dr, dc in directions:
-                    next_r = cur_r + dr
-                    next_c = cur_c + dc
+        while q:
+            cur_r, cur_c, dist = q.popleft()
 
-                    if 0 <= next_r < row and 0 <= next_c < col:
-                        if grid[next_r][next_c] == 0 and not visited[next_r][next_c]:
-                            visited[next_r][next_c] = True
-                            q.append((next_r, next_c, dist + 1))
-            return -1
-            
-        return bfs(0, 0) 
+            if cur_r == row - 1 and cur_c == col - 1:
+                shortest_path = dist
+                break
+
+            for dr, dc in directions:
+                next_r = cur_r + dr
+                next_c = cur_c + dc
+
+                if 0 <= next_r < row and 0 <= next_c < col:
+                    if grid[next_r][next_c] == 0 and not visited[next_r][next_c]:
+                        visited[next_r][next_c] = True
+                        q.append((next_r, next_c, dist + 1))
+
+        return shortest_path
