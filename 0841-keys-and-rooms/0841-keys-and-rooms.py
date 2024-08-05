f8f48da1b4b6ebdc1df84138f1fa5852cea2a99d
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-05 20:11:26 +0900

    Time: 63 ms (70.51%), Space: 16.9 MB (77.62%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,12 +1,19 @@
+from collections import deque
+
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = [False] * len(rooms)
+        visited =  [False] * len(rooms)
 
-        def dfs(cur_v):
-            visited[cur_v] = True
+        q = deque()
+        q.append(0)
+        visited[0] = True
+
+        while q:
+            cur_v = q.popleft()
+
             for v in rooms[cur_v]:
                 if not visited[v]:
-                    dfs(v)
-                    
-        dfs(0)
+                    visited[v] = True
+                    q.append(v)
+
         return False if False in visited else True
