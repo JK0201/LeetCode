45492fafb75e01c14115440297fdc785370879f8
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-07 13:19:52 +0900

    Time: 430 ms (37.67%), Space: 19 MB (21.8%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         cnt = 0
@@ -7,33 +5,27 @@ class Solution:
         visited = [[False] * col for _ in range(row)]
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        def bfs(r, c):
+        def dfs(cur_r, cur_c):
             nonlocal cnt
-            visited[r][c] = True
-            q = deque()
-            q.append((r, c))
+            visited[cur_r][cur_c] = True
 
-            while q:
-                cur_r, cur_c = q.popleft()
-                
-                for dr, dc in directions:
-                    next_r = cur_r + dr
-                    next_c = cur_c + dc
+            for dr, dc in directions:
+                next_r = cur_r + dr
+                next_c = cur_c + dc
 
-                    if 0 <= next_r < row and 0 <= next_c < col:
-                        if grid[next_r][next_c] == 0:
-                            cnt += 1
-
-                        elif grid[next_r][next_c] == 1 and not visited[next_r][next_c]:
-                            visited[next_r][next_c] = True
-                            q.append((next_r, next_c))
-
-                    else:
+                if 0 <= next_r < row and 0 <= next_c < col:
+                    if grid[next_r][next_c] == 0:
                         cnt += 1
+
+                    elif grid[next_r][next_c] == 1 and not visited[next_r][next_c]:
+                        dfs(next_r, next_c)
+
+                else:
+                    cnt += 1
 
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 1 and not visited[r][c]:
-                    bfs(r, c)
+                    dfs(r, c)
 
         return cnt
