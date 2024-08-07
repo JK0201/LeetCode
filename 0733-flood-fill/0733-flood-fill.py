2f8a6c0daccacd1335c54e5119bafd108eb389c2
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-07 18:47:14 +0900

    Time: 64 ms (75.07%), Space: 16.6 MB (87.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,28 +2,29 @@ from collections import deque
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        originalColor = image[sr][sc]
+        if originalColor == color:
+            return image
+
         row, col = len(image), len(image[0])
-        colored = [[image[r][c] for c in range(col)] for r in range(row)]
-        visited = [[False] * col for _ in range(row)]
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         def bfs(r, c):
-            visited[r][c] == True
             q = deque()
             q.append((r, c))
+            image[r][c] = color
 
             while q:
                 cur_r, cur_c = q.popleft()
-                colored[cur_r][cur_c] = color
 
                 for dr, dc in directions:
                     next_r = cur_r + dr
                     next_c = cur_c + dc
 
                     if 0 <= next_r < row and 0 <= next_c < col:
-                        if image[next_r][next_c] == image[cur_r][cur_c] and not visited[next_r][next_c]:
-                            visited[next_r][next_c] = True
+                        if image[next_r][next_c] == originalColor:
+                            image[next_r][next_c] = color
                             q.append((next_r, next_c))
 
         bfs(sr, sc)
-        return colored
+        return image
