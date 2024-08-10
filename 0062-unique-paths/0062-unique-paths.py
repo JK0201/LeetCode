377cb1d7e88993dfed9e3485517d74766d011418
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-11 02:19:58 +0900

    Time: 39 ms (31.72%), Space: 16.6 MB (33.41%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[1 if c == 0 or r == 0 else -1 for c in range(n)] for r in range(m)]
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                memo[r][c] = memo[r-1][c] + memo[r][c-1]
+                
+        return memo[m-1][n-1]
