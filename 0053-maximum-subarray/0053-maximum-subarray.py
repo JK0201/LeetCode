42cdde9f979d7797c35d1d01223d6acde32ffa94
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-15 18:04:59 +0900

    Time: 544 ms (38.04%), Space: 38.3 MB (12.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        memo = {}
-        def dp(n):
-            if n == 0:
-                return nums[n]
+        memo = {0:nums[0]}
+        for i in range(1, len(nums)):
+            memo[i] = max(nums[i], nums[i] + memo[i-1])
 
-            if n not in memo:
-                memo[n] = max(nums[n], nums[n] + dp(n-1))
-
-            return memo[n]
-
-        return max(dp(n) for n in range(len(nums)))
+        return max(memo.values())
