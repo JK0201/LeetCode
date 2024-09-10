ec42e224b9afbd40cf36aafd830fe0ac0bad93ab
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-09-10 14:12:38 +0900

    Time: 403 ms (14.27%), Space: 18.7 MB (17.5%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,43 +1,34 @@
-import heapq
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = {}
-        visited = {}
-
-        for i, t in enumerate(times):
-            if t[0] not in graph:
-                graph[t[0]] = [(t[2], t[1])]
-
-            else:
-                graph[t[0]].append((t[2], t[1]))
-
-            visited[t[0]] = visited[t[1]] = False
+        visited = [False] * n
+        graph = {k + 1 : [] for k in range(n)}
         
-        def solution(graph, start, final):
+        for t in times:
+            graph[t[0]].append((t[2], t[1]))
+
+        def dijkstra(graph, start):
             costs = {}
             pq = []
-            visited[start] = True
+            visited[start-1] = True
             heapq.heappush(pq, (0, start))
-            
+
             while pq:
                 cur_cost, cur_v = heapq.heappop(pq)
                 if cur_v not in costs:
                     costs[cur_v] = cur_cost
-                    
-                    if cur_v in graph:
-                        for cost, next_v in graph[cur_v]:
-                            next_cost = cur_cost + cost
-                            visited[next_v] = True
-                            heapq.heappush(pq, (next_cost, next_v))
-                        
+
+                    for cost, next_v in graph[cur_v]:
+                        next_cost = cur_cost + cost
+                        visited[next_v-1] = True
+                        heapq.heappush(pq, (next_cost, next_v))
+
             return costs
 
-        minCostDict = solution(graph, k, n)
+        costs = dijkstra(graph, k)        
+        
+        falseCnt = visited.count(False)
+        if falseCnt > 0:
+            return -1
 
-        for v in visited.values():
-            if v == False:
-                print(v)
-                return -1
-
-        return minCostDict[n]
+        else:
+            return max(costs.values())
